29833b124e471d1a6edac746a19f3c2b56236017
Fortunato Roncholeta <47647771+FortunatoRoncholeta@users.noreply.github.com>
2023-03-31 17:51:36 -0300

    inicio

---
+++ lexico.py
@@ -0,0 +1,29 @@
+import ply.lex as lex  # inportaçao da biblioteca 
+
+
+class MyLexxer(object):
+    reversed = {
+        'IFSULDEMINAS' : 'IFSULDEMINAS',
+        'INICIO' : 'INICIO',
+        'COMPILADORES' : 'COMPILADORES',
+        'FIM' : 'FIM',
+        'SE' : 'SE',
+        'SENAO' : 'SENAO',
+        'SENAO_SE' : 'SENAO_SE',
+        'ENQUANTO' : 'ENQUANTO',
+        'TENTAR_EXECUTAR' : 'TENTAR_EXECUTAR',
+        'PEGAR_EXCECAO' : 'PEGAR_EXCECAO',
+        'TROCAR' : 'TROCAR',
+        'CASO_FOR' : 'CASO_FOR',
+        'SAIR_REPETICAO' : 'SAIR_REPETICAO',
+        'FUNCAO': 'FUNCAO',
+        'LER' : 'LER',
+        'IMPRIMIR' : 'IMPRIMIR',
+        'RETORNA' : 'RETORNA'
+    }
+
+    tokens =[
+
+
+        
+    ]
